9697d798a063c7eb5fd34c8829000490ee79d8f8
Andrey Krasheninnikov <pchkauu@gmail.com>
2025-09-25 23:52:59 +0300

    update(http_client): Add logger interceptor

--- lib/internal/common/infrastructure/http/client/http_client.dart
+++ lib/internal/common/infrastructure/http/client/http_client.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:poeditor_utils/config/_barrel.dart';
 import 'package:poeditor_utils/internal/common/infrastructure/_barrel.dart';
+import 'package:poeditor_utils/package/observability/_barrel.dart';
 
 @singleton
 class HttpClient {
@@ -23,8 +24,12 @@ class HttpClient {
       connectTimeout: _timeout,
       receiveTimeout: _timeout,
       sendTimeout: _timeout,
+      contentType: 'application/x-www-form-urlencoded',
     );
 
     _dio.interceptors.add(_apiTokenInterceptor);
+    _dio.interceptors.add(Logger.dioInterceptor);
   }
+
+  Dio get send => _dio;
 }

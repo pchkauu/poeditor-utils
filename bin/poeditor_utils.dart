12ccbcbed0aa8c659be04ac7cd1b1a1f644ebfd8
Andrey Krasheninnikov <pchkauu@gmail.com>
2025-09-25 21:56:38 +0300

    add(all): Init project

---
+++ bin/poeditor_utils.dart
@@ -0,0 +1,5 @@
+import 'package:poeditor_utils/poeditor_utils.dart' as poeditor_utils;
+
+void main(List<String> arguments) {
+  print('Hello world: ${poeditor_utils.calculate()}!');
+}
---
+++ test/poeditor_utils_test.dart
@@ -0,0 +1,8 @@
+import 'package:poeditor_utils/poeditor_utils.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

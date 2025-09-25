3b779ab8240ed8f2f47d2bfe53a1fb1f218283f9
Andrey Krasheninnikov <pchkauu@gmail.com>
2025-09-25 21:59:19 +0300

    update(bin,lib): Clear templates

--- bin/poeditor_utils.dart
+++
@@ -1,5 +0,0 @@
-import 'package:poeditor_utils/poeditor_utils.dart' as poeditor_utils;
-
-void main(List<String> arguments) {
-  print('Hello world: ${poeditor_utils.calculate()}!');
-}
--- lib/poeditor_utils.dart
+++ lib/poeditor_utils.dart
@@ -1,3 +1,4 @@
-int calculate() {
-  return 6 * 7;
-}
+/// This library provides utilities for working with POEditor API and managing
+/// translation files, including downloading, uploading, and processing
+/// localization data.
+library;
